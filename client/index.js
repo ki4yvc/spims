f5cccbd2b7fdeab5d00ccb147ebf48e7cc039482
Adam Greene <adam@adgreene.net>
2015-12-19 18:25:49 -0500

    Makes people table load dynamically

--- client/index.js
+++ client/index.js
@@ -4,13 +4,7 @@ var userName = "";
 var userPosition = "";
 var pages = [];
 
-var accountMenu;
-var currentUser;
-var logOutButton;
-var content;
-
 window.onload = function() {
-	setElements();
 	logIn("00378522", "aGreene5764"); // Would use whatever the user input
 }
 
@@ -19,6 +13,9 @@ function setElements() {
 	currentUser = document.getElementById('current-user');
 	logOutButton = document.getElementById('log-out-button');
 	content = document.getElementById('content');
+	if (pages.indexOf("people") > -1) {
+		peopleTableHeaderData = document.getElementById('people-table').children[0].getElementsByTagName('td');
+	}
 }
 
 function logIn(userID, userPassword) {
@@ -28,9 +25,9 @@ function logIn(userID, userPassword) {
 	this.userPassword = userPassword;
 	userName = "Adam Greene"; // or whatever the XHR returns
 	userPosition = "Admin"; // or whatever the XHR returns
-	// It also returns the HTML for the available pages
-	pages = [["people", "<div id='people-page' class='page'></div>"],
-			["inventory", "<div id='inventory-page' class='page'></div>"]];
+	// It also returns the available pages
+	pages = ["people", "inventory", "equipment"];
+	loadPage(pages[0]);
 	setUp();
 }
 
@@ -39,12 +36,49 @@ function logOut() {
 }
 
 function setUp() {
+	setElements();
 	currentUser.className = currentUser.className + " " +
 			userPosition.toLowerCase();
 	currentUser.innerHTML = userName;
 	currentUser.title = "User is " + userPosition;
-
 	addEventListeners();
+}
+
+function loadPage(pageName) {
+	// Do XHR to load page data
+	// The rest assumes that it successfully loaded people
+	var peopleData = [{"name": "Greene, Adam, D","id": "00000000","position": "Admin","address": "1600 Pennsylvania Ave NW, Washington, DC 20500","phone": "(202) 456-1111","email": "greene.adam.d@gmail.com"}]
+	var peopleTable = document.createElement('table');
+	peopleTable.id = "people-table";
+	var thead = document.createElement('thead');
+	thead.innerHTML = "<td class='sorting-by'>Name</td><td>ID</td><td>Position</td><td>Address</td><td>Phone</td><td>Email</td>";
+	peopleTable.appendChild(thead);
+	var tbody = document.createElement('tbody');
+	for (var i = 0; i < peopleData.length; i++) {
+		var row = document.createElement('tr');
+		var td = (document.createElement('td'));
+		td.innerHTML = peopleData[i].name;
+		row.appendChild(td);
+		td = (document.createElement('td'));
+		td.innerHTML = peopleData[i].id
+		row.appendChild(td);
+		td = (document.createElement('td'));
+		td.innerHTML = "<span>" + peopleData[i].position + "</span>"
+		td.children[0].className = "position-text " + peopleData[i].position.toLowerCase();
+		row.appendChild(td);
+		td = (document.createElement('td'));
+		td.innerHTML = peopleData[i].address
+		row.appendChild(td);
+		td = (document.createElement('td'));
+		td.innerHTML = peopleData[i].phone
+		row.appendChild(td);
+		td = (document.createElement('td'));
+		td.innerHTML = peopleData[i].email
+		row.appendChild(td);
+		tbody.appendChild(row);
+	}
+	peopleTable.appendChild(tbody);
+	content.appendChild(peopleTable);
 }
 
 function addEventListeners() {
@@ -52,13 +86,29 @@ function addEventListeners() {
 		toggleClass(accountMenu, 'selected');
 	});
 	logOutButton.addEventListener('click', logOut);
+	if (pages.indexOf("people") > -1) {
+		for (var i = 0; i < peopleTableHeaderData.length; i++) {
+			peopleTableHeaderData[i].addEventListener('click', sortTableBy);
+		}
+	}
+}
+
+function sortTableBy() {
+	if (this.className != "sorting-by") {
+		for (var i = 0; i < peopleTableHeaderData.length; i++) {
+			if (peopleTableHeaderData[i].className == "sorting-by") {
+				peopleTableHeaderData[i].className = "";
+				break;
+			}
+		}
+		this.className = "sorting-by";
+	}
 }
 
 function toggleClass(element, className) {
 	var index = element.className.indexOf(className);
 	if (index > -1) {
-		var newClassName = element.className.substring(0, index) +
-				element.className.substring(index + className.length);
+		var newClassName = element.className.substring(0, index) + element.className.substring(index + className.length);
 		element.className = newClassName.replace(/\s+/g, " ").trim();
 	} else {
 		element.className += " " + className;
