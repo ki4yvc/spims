38c03f04fbb1af1c7add4cc9c9cb26317c484708
Adam Greene <adam@adgreene.net>
2016-01-10 18:00:02 -0500

    Updates variable delimiters using syntax similar to swift

--- client/api/api.py
+++ client/api/api.py
@@ -8,10 +8,21 @@ import mimetypes
 # This is just an example, if you decide to use python, CGI is apparently discouraged, look into WSGI instead
 path = os.environ["REQUEST_URI"]
 qstr = ""
+
+pageVars = {
+	"cname": "Whitmire Rescue Squad",
+	"cid": "00000000",
+	"uid": "00000000",
+	"uname": "Adam Greene",
+	"themecolor100": "#009fff",
+	"themecolor75": "#0077bf",
+	"themecolor50": "#005080"
+}
+
 if "?" in path:
 	qstr = path.split("?")[1]
 	path = path.split("?")[0]
-path = "/index.html" if path == "/" else path
+if path == "/": path = "/index.html"
 
 def printContentType():
 	mime = mimetypes.guess_type(path)[0]
@@ -19,11 +30,13 @@ def printContentType():
 		pass
 	print("Content-type:", mime, end="\n\n")
 
-printContentType()
-with open(".." + path, encoding = "utf8") as page:
-	pagestr = page.read()
+def fillPage(match):
+	var = match.group(0)[2:-1]
+	return pageVars[var]
 
-if path == "/index.html":
-	print(pagestr.format("Whitmire Rescue Squad", cid = "00000000", uid = "00000000", uname = "Adam Greene"))
-else:
-	print(pagestr)
+printContentType()
+with open(".." + path) as page:
+	pagestr = page.read()
+pagestr = re.sub(r"\\\(.*?\)", fillPage, pagestr)
+
+print(pagestr)
