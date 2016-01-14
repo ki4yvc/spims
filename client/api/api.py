92c2bf54b199a98df4fa36ca67033205c7467684
Adam Greene <adam@adgreene.net>
2016-01-13 21:27:30 -0500

    Revert "includes what Nathan didnt put on github but the server"
    This reverts commit 0d4105e325ea2880615e384ef29f5ff6aa9d671c.

--- client/api/api.py
+++ client/api/api.py
@@ -1,4 +1,4 @@
-#!/home/dekann/Python-3.5.1/python
+#!C:/Users/adgreene/AppData/Local/Programs/Python/Python35-32/python.exe
 import sys
 import os
 import cgi
@@ -13,7 +13,7 @@ pageVars = {
 	"cname": "Whitmire Rescue Squad",
 	"cid": "00000000",
 	"uid": "00000000",
-	"uname": "David Andrews Jr.",
+	"uname": "Adam Greene",
 	"themecolor100": "#009fff",
 	"themecolor75": "#0077bf",
 	"themecolor50": "#005080"
