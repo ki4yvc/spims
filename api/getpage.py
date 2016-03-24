0dfde4e21f995cd1a06e7c7a03cb954b7dbdb495
Nicholas  Gustafson <nicholasgustafson@Nicholass-MacBook-Pro.local>
2016-03-24 15:15:37 -0400

    adding get, delete and addColumn methods

--- api/getpage.py
+++ api/getpage.py
@@ -12,15 +12,39 @@ conn = pymysql.connect(
     user='root',
     passwd='spimsMySQL2015',
     host='localhost')
-try:
-    with conn.cursor() as cursor:
-        cursor.execute("INSERT INTO `people` (`created`, `firstName`, `lastName`, `radioID`, `position`, `address`, `city`, `state`, `zip`, `phone`, `email`) VALUES (NOW(), 'Nicholas', 'Gustafson', '1234', 'Hitler', '001 Hollywood Rd', 'Columbia', 'SC', 29212, '8033157023', 'nichgus7@gmail.com')")
-        conn.commit()
-finally:
-    conn.close()
+#Putting into the server (concret)
+def add(db, fN, lN, rID, p, a, c, s, z, ph, e):
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("INSERT INTO"+db+"(`created`, `firstName`, `lastName`, `radioID`, `position`, `address`, `city`, `state`, `zip`, `phone`, `email`) VALUES (NOW(),"+fN+","+lN+","+rID+","+p+","+a+","+c+","+s+","+z+","+ph+","+e+")")
+            conn.commit()
+        finally:
+            conn.close()
 
+#Taking out from the server (concret)
+def get(db):
+    try:
+        with conn.cursor() as cursor:
+            return cursor.execute("SELECT * FROM"+db)
+        finally:
+            conn.close()
 
-# This would be done dynamically using the qstr and SQL database
+def delete(db, column, value):
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("DELETE FROM "+db+" WHERE "+column+" IS "+value)
+            conn.commit()
+        finally:
+            conn.close()
+
+def addColumn(db, newName, dataType):
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("ALTER TABLE "+db+" ADD COLUMN "+newName+" "+dataType)
+            conn.commit()
+        finally:
+            conn.close()
+
 with open("../page.json") as rsrc:
 	content = rsrc.read()
 print(content, end="")
