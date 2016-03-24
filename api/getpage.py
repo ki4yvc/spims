73e349d58db49783eadf5721b7fc50bef6dbb73f
Nicholas  Gustafson <nicholasgustafson@Nicholass-MacBook-Pro.local>
2016-03-24 15:27:07 -0400

    adding get, delete and addColumn methods, fixing syntax errors

--- api/getpage.py
+++ api/getpage.py
@@ -14,35 +14,29 @@ conn = pymysql.connect(
     host='localhost')
 #Putting into the server (concret)
 def add(db, fN, lN, rID, p, a, c, s, z, ph, e):
-    try:
-        with conn.cursor() as cursor:
-            cursor.execute("INSERT INTO"+db+"(`created`, `firstName`, `lastName`, `radioID`, `position`, `address`, `city`, `state`, `zip`, `phone`, `email`) VALUES (NOW(),"+fN+","+lN+","+rID+","+p+","+a+","+c+","+s+","+z+","+ph+","+e+")")
-            conn.commit()
-            conn.close()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO"+db+"(`created`, `firstName`, `lastName`, `radioID`, `position`, `address`, `city`, `state`, `zip`, `phone`, `email`) VALUES (NOW(),"+fN+","+lN+","+rID+","+p+","+a+","+c+","+s+","+z+","+ph+","+e+")")
+    conn.commit()
+    conn.close()
 
 #Taking out from the server (concret)
 def get(db):
-    try:
-        with conn.cursor() as cursor:
-            return cursor.execute("SELECT * FROM"+db)
-        finally:
-            conn.close()
+    cursor = con.cursor()
+    return cursor.execute("SELECT * FROM"+db)
+    conn.close()
 
 def delete(db, column, value):
-    try:
-        with conn.cursor() as cursor:
-            cursor.execute("DELETE FROM "+db+" WHERE "+column+" IS "+value)
-            conn.commit()
-        finally:
-            conn.close()
+    cursor = con.cursor()
+    cursor.execute("DELETE FROM "+db+" WHERE "+column+" IS "+value)
+    conn.commit()
+    conn.close()
 
 def addColumn(db, newName, dataType):
-    try:
-        with conn.cursor() as cursor:
-            cursor.execute("ALTER TABLE "+db+" ADD COLUMN "+newName+" "+dataType)
-            conn.commit()
-        finally:
-            conn.close()
+    cursor = con.cursor()
+    cursor.execute("ALTER TABLE "+db+" ADD COLUMN "+newName+" "+dataType)
+    conn.commit()
+    conn.close()
+
 
 with open("../page.json") as rsrc:
 	content = rsrc.read()
