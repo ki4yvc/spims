c409d1ad921ac229462730e575b7964aaaad9820
Adam Greene <greene.adam.d@gmail.com>
2016-04-08 11:07:58 -0400

    doesnt work but its something

--- api/addperson.py
+++ api/addperson.py
@@ -5,9 +5,12 @@ conn = pymysql.connect(
     db='spims',
     user='root',
     passwd='spimsMySQL2015',
-    host='localhost')
+    host='104.196.114.233')
 cursor = conn.cursor()
 
 def insert(dbase, n, rID, p, a, ph, e):
     cursor.execute('INSERT INTO %s (`created`, `name`, `radioID`, `position`, `address`, `phone`, `email`) values (NOW(),%s,%s,%s,%s,%s,%s)' % (str(dbase), str(n), str(rID), str(p), str(a), str(ph), str(e)))
     conn.commit()
+
+insert("whitmire_people", "Trump", "666", "Supreme Leader", "700 Place Avenue", "1234567890", "hi@person.com")
+conn.close()
